ee04ca381fd92ea8c8614c1854393288a5bffd64
sumit-597 <sumitv0611@gmail.com>
2024-11-26 18:41:15 +0530

    Update index.js

--- Project/src/assets/index.js
+++ Project/src/assets/index.js
@@ -1,7 +1,1 @@
-import linkIcon from "./link.svg"
-import loader from "./loader.svg"
-import copy from "./copy.svg"
-import logo from "./logo_.png"
-import tick from "./tick.svg"
-
-export { linkIcon, loader, copy, logo, tick }
+.
